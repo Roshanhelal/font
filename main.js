ba0a30528dacff056edbbe747c24534b7c14db62
Roshanhelal <98272607+Roshanhelal@users.noreply.github.com>
2022-08-20 21:06:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var noseX,noseY,rightWristX,leftWristX,difference;
+
 function setup(){
     video=createCapture(VIDEO);
     video.size(550, 500);
@@ -16,5 +18,22 @@ function modelLoaded(){
 function gotposes(results){
 if(results.length>0){
     console.log(results);
+noseX=results[0].pose.nose.x;
+noseY=results[0].pose.nose.y;
+rightWristX=results[0].pose.rightWrist.x;
+leftWristX=results[0].pose.leftWrist.x;
+console.log("noseX="+noseX+"noseY="+noseY);
+console.log("rightwristX="+rightWristX+"leftwristX="+leftWristX);
 }
 }
+
+function draw(){
+    background("#969A97");
+    difference=floor(leftWristX-rightWristX);
+    fill("#34a2eb");
+    stroke("#34a2eb")
+   text("Roshan",noseX,noseY);
+  textSize(difference);
+    textAlign(CENTER);
+}
+
